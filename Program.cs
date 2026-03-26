a292d6a3478f7d4b8651bce3a3276b5147ad0c10
Meu Nome <ryan.simao72@gmail.com>
2026-03-25 22:45:35 -0300

    FEITO

--- Program.cs
+++ Program.cs
@@ -1,4 +1,9 @@
-﻿Console.BackgroundColor = ConsoleColor.White;
-Console.ForegroundColor = ConsoleColor.Black;
-Console.WriteLine("Corinthians");
+﻿Console.BackgroundColor = ConsoleColor.Black;
+Console.ForegroundColor = ConsoleColor.White;
+Console.Write(" C o r i n");
+
+Console.BackgroundColor = ConsoleColor.Black;
+Console.BackgroundColor = ConsoleColor.White;
+Console.Write(" t h i a n s");
+
 Console.ResetColor();
